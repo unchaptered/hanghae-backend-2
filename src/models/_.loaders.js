78b252352f262dce60386614d72bad90a186e713
Unchaptered <workstation19961002@gmail.com>
2022-07-29 20:20:31 +0900

    Create `*/models/_.loaders.js`

---
+++ src/models/_.loaders.js
@@ -0,0 +1,16 @@
+import Env from './env/env.js';
+import BasicEnv from './env/basic.env.js';
+import DatabaseEnv from './env/database.env.js';
+
+import classIterator from './iterator/class.iterator.js';
+
+
+export {
+    
+    classIterator,
+
+    Env,
+    BasicEnv,
+    DatabaseEnv
+
+}
