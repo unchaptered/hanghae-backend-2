2488593efacc8e700397769360031e79d2ad0d1d
Unchaptered <workstation19961002@gmail.com>
2022-07-30 15:55:44 +0900

    Update `*/controllers/_.export.js`, exporter 제작

--- src/layers/controllers/_.export.js
+++ src/layers/controllers/_.export.js
@@ -1,9 +1,14 @@
 import * as GlobalController from './global.controller.js';
-
+import * as AuthController from './auth.controller.js';
+import * as BoardController from './board.controller.js';
+import * as CommentController from './comment.controller.js';
 
 export {
     
     GlobalController,
+    AuthController,
+    BoardController,
+    CommentController
 
 }
 
