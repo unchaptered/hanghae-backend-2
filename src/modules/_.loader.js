87ced2cbe469d7286a249f76e669a589bcd32756
Unchaptered <workstation19961002@gmail.com>
2022-07-31 01:22:46 +0900

    Update `*/modules/_.loader.js`, 신규 모듈 추가

--- src/modules/_.loader.js
+++ src/modules/_.loader.js
@@ -1,18 +1,24 @@
-import EnvProvider from './providers/env.provider.js'
+import BaseModule from './base/base.module.js';
+
+import EnvProvider from './providers/env.provider.js';
 import DatabaseProvider from './providers/database.provider.js';
 
 import FormFactory from './factories/form.factory.js';
+import JoiValidator from './validator/joi.validator.js';
 
 import { exceptionHandler } from './handlers/exception.handler.js';
 import { exceptionMiddleware } from './middlewares/exception.middleware.js';
 
 
 export {
+
+    BaseModule,
     
     EnvProvider,
     DatabaseProvider,
 
     FormFactory,
+    JoiValidator,
 
     exceptionHandler,
     exceptionMiddleware,
