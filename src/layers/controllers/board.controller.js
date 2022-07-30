d6670fa111731490a7acd496ab3bd7630a8a28c7
Unchaptered <workstation19961002@gmail.com>
2022-07-31 02:12:48 +0900

    Create `**/board.controller.js`, 글쓰기 및  특정 게시글 보기/수정/삭제

--- src/layers/controllers/board.controller.js
+++ src/layers/controllers/board.controller.js
@@ -1,21 +1,106 @@
+import { FormFactory, JoiValidator } from '../../modules/_.loader.js';
+import { BoardEntity } from '../../models/entity/_.export.js';
+import { BoardDto } from '../../models/dtos/_.export.js';
+import Joi from 'joi';
+
 export const getAllBoard = (req, res, next) => {
     return res.json('get all board');
 }
 
-export const postBoard = (req, res, next) => {
-    return res.json('post board');
+export const postBoard = async (req, res, next) => {
+
+    try {
+
+        const boardDto = new BoardDto({ ...req.body });
+
+        const joiValidator = new JoiValidator();
+        const result = await joiValidator.validate(boardDto, boardDto._getJoiInstance());
+        
+        const formFactory = new FormFactory();
+        return res.json(
+            formFactory.getSuccessForm('게시글 작성에 성공하셨습니다.', result));
+
+    } catch(err) {
+        
+        res.locals.error = err;
+
+        return next();
+
+    }
+
 }
 
-export const getBoardById = (req, res, next) => {
-    return res.json('get baord by boardId');
+export const getBoardById = async (req, res, next) => {
+
+    try {
+     
+        const { boardId } = req.params;
+
+        const joiValidator = new JoiValidator();
+        const result = await joiValidator.validate({ boardId }, { boardId: Joi.number() });
+        
+        const formFactory = new FormFactory();
+        return res.json(
+            formFactory.getSuccessForm('게시글 불러오기에 성공했습니다.', result));
+
+    } catch(err) {
+
+        res.locals.error = err;
+
+        return next();
+
+    }
+
 }
 
-export const putBoardById = (req, res, next) => {
-    return res.json('put baord by boardId');
+export const putBoardById = async (req, res, next) => {
+
+    try {
+     
+        const { boardId } = req.params;
+        const boardDto = new BoardDto({ ...req.body });
+        boardDto.setBoardId = boardId;
+
+        const boardJoi = boardDto._getJoiInstance();
+        boardJoi.boardId = boardJoi.boardId.required();
+
+        const joiValidator = new JoiValidator();
+        const result = await joiValidator.validate(boardDto, boardJoi);
+        
+        const formFactory = new FormFactory();
+        return res.json(
+            formFactory.getSuccessForm('게시글 수정에 성공했습니다.', result));
+
+    } catch(err) {
+
+        res.locals.error = err;
+
+        return next();
+
+    }
 }
 
-export const delBoardById = (req, res, next) => {
-    return res.json('delete baord by boardId');
+export const delBoardById = async (req, res, next) => {
+
+    try {
+     
+        const { boardId } = req.params;
+
+        const joiValidator = new JoiValidator();
+        const result = await joiValidator.validate({ boardId }, { boardId: Joi.number() });
+        
+        const formFactory = new FormFactory();
+        return res.json(
+            formFactory.getSuccessForm('게시글 삭제에 성공했습니다.', result));
+
+    } catch(err) {
+
+        res.locals.error = err;
+
+        return next();
+
+    }
+
 }
 
 export const increaseBoardLike = (req, res, next) => {
