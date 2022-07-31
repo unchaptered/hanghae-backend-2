dc77d228a9c57b5aaeee6b32f1b04c7b49e43c6e
Unchaptered <workstation19961002@gmail.com>
2022-07-31 16:20:59 +0900

    Create `**/comment.dto.js`

--- src/models/dtos/_.export.js
+++ src/models/dtos/_.export.js
@@ -1,12 +1,15 @@
 import UserJoinDto from './user/user.login.dto.js';
 import UserLoginDto from './user/user.join.dto.js';
 import BoardDto from './board/board.dto.js';
+import CommentDto from './comment/comment.dto.js';
 
 export {
 
     UserJoinDto,
     UserLoginDto,
 
-    BoardDto
+    BoardDto,
+    
+    CommentDto
     
 }
