e26d5a64fb94567168d489ed051af34fbc185e86
Unchaptered <workstation19961002@gmail.com>
2022-07-31 16:21:49 +0900

    Update `**/comment.controller.js`, 댓글 달기 및 기존 댓글 수정/삭제 구현

--- src/layers/controllers/comment.controller.js
+++ src/layers/controllers/comment.controller.js
@@ -1,21 +1,87 @@
+import { FormFactory, JoiValidator } from '../../modules/_.loader.js';
+import { CommentDto } from '../../models/dtos/_.export.js';
 
 /** @param { Request } req @param { Response } res @param { NextFunction } next */
-export const getAllCommentByBoardId = (req, res, next) => {
+export const getAllCommentByBoardId = async (req, res, next) => {
 }
 
 /** @param { Request } req @param { Response } res @param { NextFunction } next */
-export const postComment = (req, res, next) => {
+export const postComment = async (req, res, next) => {
+
+    try {
+
+        const commentDto = new CommentDto({ ...req.body });
+        
+        const result = await new JoiValidator().validate(commentDto, commentDto._getJoiInstance());
+        
+        const formFactory = new FormFactory();
+        return res.json(
+            formFactory.getSuccessForm('댓글 작성에 성공하셨습니다.', result));
+
+    } catch (err) {
+        
+        res.locals.error = err;
+
+        return next();
+
+    }
+
 }
 
 /** @param { Request } req @param { Response } res @param { NextFunction } next */
-export const putCommentByCommentId = (req, res, next) => {
+export const putCommentByCommentId = async (req, res, next) => {
+
+    try {
+
+        const { commentId } = req.params;
+        const commentDto = new CommentDto({ ...req.body });
+        commentDto.setCommentId = commentId;
+
+        const commnetJoi = commentDto._getJoiInstance();
+        commnetJoi.commentId = commnetJoi.commentId.required();
+
+        const joiValidator = new JoiValidator();
+        const result = await joiValidator.validate(commentDto, commnetJoi);
+
+        const formFactory = new FormFactory();
+        return res.json(
+            formFactory.getSuccessForm('댓글 수정에 성공했습니다.', result));
+
+        
+    } catch (err) {
+        
+        res.locals.error = err;
+
+        return next();
+
+    }
 }
 
 /** @param { Request } req @param { Response } res @param { NextFunction } next */
-export const delCommentByComment = (req, res, next) => {
+export const delCommentByComment = async (req, res, next) => {
+
+    try {
+
+        const { commentId } = req.params;
+        
+        const joiValidator = new JoiValidator();
+        const result = await joiValidator.validate({ commentId }, { commentId: Joi.number() });
+        
+        const formFactory = new FormFactory();
+        return res.json(
+            formFactory.getSuccessForm('댓글 삭제에 성공했습니다.', result));
+
+    } catch(err) {
+        
+        res.locals.error = err;
+
+        return next();
+
+    }
+
 }
 
 /** @param { Request } req @param { Response } res @param { NextFunction } next */
-export const increaseCommetLikeByCommentId = (req, res, next) => {
+export const increaseCommetLikeByCommentId = async (req, res, next) => {
 }
 
