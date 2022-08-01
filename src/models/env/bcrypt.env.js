d11ecbe98e539681300f77303af8c349abd595eb
Unchaptered <workstation19961002@gmail.com>
2022-08-01 17:39:29 +0900

    Create `**/bcrypt.env.js`, 암호화 환경 변수 추가

---
+++ src/models/env/bcrypt.env.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { classIterator } from '../_.loader.js';
+
+/**
+ * Bcrypt 공급자 클래스를 가동시키기 위한 최소한의 환경 변수 입니다.
+ */
+export default class BcryptEnv {
+
+    /** @type { number } */
+    SALT;
+
+    [Symbol.iterator]
+
+    constructor() {
+        
+        this.SALT = +process.env.SALT;
+        this[Symbol.iterator] = classIterator;
+
+    }
+}
