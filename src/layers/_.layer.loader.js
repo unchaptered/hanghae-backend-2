100202e1a7d998d6e78a5bfdb7ae7d964c3a99ea
Unchaptered <workstation19961002@gmail.com>
2022-08-04 11:08:16 +0900

    Apply `**/_.layer.loader.js`, last 5 changes

--- src/layers/_.layer.loader.js
+++ src/layers/_.layer.loader.js
@@ -1,11 +1,14 @@
 import * as AuthController from './auth/auth.controller.js';
 import * as AuthService from './auth/auth.service.js';
+import * as AuthRepository from './auth/auth.repository.js';
 
 import * as BoardController from './board/board.controller.js';
 import * as BoardService from './board/board.service.js';
+import * as BoardRepository from './board/board.repository.js';
 
 import * as CommentController from './comment/comment.controller.js';
 import * as CommentService from './comment/comment.service.js';
+import * as CommentRepository from './comment/comment.repository.js';
 
 import * as GlobalController from './global/global.controller.js';
 
@@ -14,12 +17,15 @@ export {
 
     AuthController,
     AuthService,
+    AuthRepository,
 
     BoardController,
     BoardService,
+    BoardRepository,
 
     CommentController,
     CommentService,
+    CommentRepository,
 
     GlobalController
 
