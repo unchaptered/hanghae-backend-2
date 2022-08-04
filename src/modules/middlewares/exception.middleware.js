bb5a3b2a0563646efc3acd12f6a0bd5812ac26ce
Unchaptered <workstation19961002@gmail.com>
2022-08-04 16:31:25 +0900

    Update `**/exception.middlewares.js`, 404 페이지 문구 수정

--- src/modules/middlewares/exception.middleware.js
+++ src/modules/middlewares/exception.middleware.js
@@ -9,10 +9,17 @@ import { FormFactory, exceptionHandler } from '../_.loader.js';
 export const exceptionMiddleware = (req, res, next) => {
 
     const formFactory = res.locals.formFactory ?? new FormFactory();
-    const error = res.locals.error;
+
+    if (Object.values(res.locals).length === 0)
+        return res.status(404).json(
+            new FormFactory().getFailureForm('지원하지 않는 경로입니다.'));
+    
+    const error = res.locals?.error;
+    if (!error)
+        return res.status(500).json(
+            new FormFactory().getFailureForm('알 수 없는 에러가 발생하였습니다. (핸들러 에러)'));
 
     const except = exceptionHandler(error);
-
     return res.status(except.statusCode).json(
         formFactory.getFailureForm(except.message, {}));
 
